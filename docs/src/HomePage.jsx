1a033a77fed2a65c9d08a6b82889d7c83f27d379
Jodie Doubleday <jodie.doubleday@holidayextras.com>
2015-04-02 14:38:51 +0100

    adding sections and headers for each section

--- docs/src/HomePage.jsx
+++ docs/src/HomePage.jsx
@@ -18,16 +18,21 @@ var HomePage = React.createClass({
           <div className="container">
             <img src="./assets/toolkit-logo.svg" alt="Toolkit" />
             <h1>{packageJSON.description}</h1>
+            <p className="lead">A CSS Framework and a Set of React Components that extend from Bootstrap</p>
           </div>
         </header>
         <div className="container">
-          <p className="lead">This UI toolkit comprises of extensions we feel are necessary to make bootstrap better</p>
-          
-          <h2 id="button" className="page-header">Ultimate Button</h2>
-          <CustomComponent codeText={fs.readFileSync(__dirname + '/../examples/Button.jsx', 'utf8')} />
+          <section>
+            <h2 className="page-header">Getting started</h2>
+          </section>
+          <section className=""> 
+            <h2 className="page-header">Components</h2>    
+            <h3 id="button">Ultimate Button</h3>
+            <CustomComponent codeText={fs.readFileSync(__dirname + '/../examples/Button.jsx', 'utf8')} />
 
-          <h2 id="badge" className="page-header">Bootstrap badge</h2>
-          <CustomComponent codeText={fs.readFileSync(__dirname + '/../examples/BootstrapBadge.jsx', 'utf8')} />
+            <h3 id="badge">Bootstrap badge</h3>
+            <CustomComponent codeText={fs.readFileSync(__dirname + '/../examples/BootstrapBadge.jsx', 'utf8')} />
+          </section>
         </div>
         <PageFooter />
       </div>
