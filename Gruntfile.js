40c753f7db65f1b101a883d0c8ee92d72319f18d
jackdcrawford <jackcrawford19@gmail.com>
2015-04-16 12:10:42 +0100

    UXUI-204 tidy whitespace

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,9 @@
 module.exports = function(grunt) {
   var gtx = require('gruntfile-gtx').wrap(grunt);
-    gtx.loadAuto();
+  gtx.loadAuto();
 
   var gruntConfig = require('./grunt');
-    gruntConfig.package = require('./package.json');
+  gruntConfig.package = require('./package.json');
 
   gtx.config(gruntConfig);
 
