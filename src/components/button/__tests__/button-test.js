9ae18e5c6419f933b18aaf5e10e4f4b8c3bc0904
Jodie Doubleday <jodie.doubleday@holidayextras.com>
2015-05-05 16:20:24 +0100

    getting tests to use testutils and findRenderedDOMComponentWithClass

--- src/components/button/__tests__/button-test.js
+++ src/components/button/__tests__/button-test.js
@@ -1,6 +1,7 @@
 /** @jsx React.DOM */
 
 jest.dontMock('../code/views/buttonView.jsx');
+jest.dontMock('classnames');
 
 var React = require('react/addons');
 var ButtonView = require('../code/views/buttonView.jsx');
@@ -8,8 +9,44 @@ var TestUtils = React.addons.TestUtils;
 
 describe('ButtonComponent', function() {
 
-  it('is an element', function() {
-    expect(TestUtils.isElement(<ButtonView />)).toBeTruthy();
+  it('should render button', function() {
+    var button = TestUtils.renderIntoDocument(
+      <ButtonView>Book Now</ButtonView>
+    );
+
+    var renderedButton = TestUtils.findRenderedDOMComponentWithClass(button, 'component-button');
+    expect(renderedButton.getDOMNode().textContent).toEqual('Book Now');
+
+  });
+
+  it('should render a primary button', function() {
+    var primaryButton = TestUtils.renderIntoDocument(
+      <ButtonView purpose="primary">Book Now</ButtonView>
+    );
+
+    var renderedButton = TestUtils.findRenderedDOMComponentWithClass(primaryButton, 'primary');
+    expect(renderedButton).toBeDefined();
+
+  });
+
+  it('should render a large button', function() {
+    var largeButton = TestUtils.renderIntoDocument(
+      <ButtonView size="large">Book Now</ButtonView>
+    );
+
+    var renderedButton = TestUtils.findRenderedDOMComponentWithClass(largeButton, 'large');
+    expect(renderedButton).toBeDefined();
+
+  });
+
+  it('should render a small secondary button', function() {
+    var smallSecondaryButton = TestUtils.renderIntoDocument(
+      <ButtonView size="small" purpose="secondary">Book Now</ButtonView>
+    );
+
+    var renderedButton = TestUtils.findRenderedDOMComponentWithClass(smallSecondaryButton, 'small secondary');
+    expect(renderedButton).toBeDefined();
+
   });
 
 });
--- src/components/flag/code/templates/flagTemplate.jsx
+++ src/components/flag/code/templates/flagTemplate.jsx
@@ -3,7 +3,7 @@ var classNames = require('classnames');
 var getComponentClasses = require('../../../../utils/getComponentClasses');
 
 module.exports = function(props) {
-  
+
   var propClasses = ['size', 'purpose', 'position'];
   var classes = getComponentClasses('component-flag', propClasses, props);
 
