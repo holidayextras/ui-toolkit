03d751f7e874fdf82b326d79e684bf0e42e12bba
Jodie Doubleday <jodie.doubleday@holidayextras.com>
2015-05-07 12:18:20 +0100

    trimming whitepace

--- src/utils/__tests__/getComponentClasses-test.js
+++ src/utils/__tests__/getComponentClasses-test.js
@@ -3,26 +3,26 @@ jest.dontMock('../getComponentClasses.js');
 var getComponentClasses = require('../getComponentClasses.js');
 
 describe('getComponentClasses', function() {
-  
+
   describe('with invalid arguments', function() {
-    
+
     it('display and error message when no arguments', function() {
       expect(function() {
-        getComponentClasses()
+        getComponentClasses();
       }).toThrow('Invalid default class');
     });
 
     it('display and error message when argument it not a strong', function() {
       expect(function() {
-        getComponentClasses(1)
+        getComponentClasses(1);
       }).toThrow('Invalid default class');
-    });   
+    });
 
     it('returns default class when propClasses is not an Array', function() {
       var result = getComponentClasses('component-button', 1);
       var expected = ['component-button'];
       expect(result).toEqual(expected);
-    }); 
+    });
 
     it('returns default class when props is not an Object', function() {
       var result = getComponentClasses('component-button', ['foo'], 1);
