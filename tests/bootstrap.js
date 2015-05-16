ff12baf6b7bc05394e3f35d92706bf2672e3b657
Joe Stump <joe@joestump.net>
2015-05-16 14:43:11 -0700

    Got a working implementation of Mocha/Chai with jsdom.

--- tests/bootstrap.js
+++ tests/bootstrap.js
@@ -6,6 +6,6 @@ global.TestUtils = React.addons.TestUtils;
 
 // Virtual DOM bootstrap.
 global.jsdom = require('jsdom');
-global.document = jsdom();
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = document.parentWindow;
 global.navigator = window.navigator;
