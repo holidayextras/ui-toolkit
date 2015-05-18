f0cd5829bd785b430be4dec7fd0b24d05d77a913
Nicki Lever <nicki.lever@holidayextras.com>
2015-05-18 09:41:43 +0100

    allow onClick props

--- src/components/button/code/templates/buttonTemplate.jsx
+++ src/components/button/code/templates/buttonTemplate.jsx
@@ -8,7 +8,7 @@ module.exports = function(props) {
   var classes = getComponentClasses('component-button', propClasses, props);
 
   return (
-    <button className={classNames(classes)} disabled={props.disabled} type={props.type}>
+    <button className={classNames(classes)} disabled={props.disabled} type={props.type} onClick={props.onClick}>
       {props.children}
     </button>
   );
