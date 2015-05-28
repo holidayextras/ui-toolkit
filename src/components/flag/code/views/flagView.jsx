7d8a9a9d70b6c9b59a715330ca502f054a680394
Jodie Doubleday <jodie.doubleday@holidayextras.com>
2015-05-28 17:02:10 +0100

    bring in lodash modules, fix typo

--- src/components/flag/code/views/flagView.jsx
+++ src/components/flag/code/views/flagView.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 module.exports = React.createClass({
 
   propTypes: {
-    purpose: React.PropTypes.oneOf(['defult', 'primary', 'secondary', 'success', 'warning', 'danger', 'info']),
+    purpose: React.PropTypes.oneOf(['default', 'primary', 'secondary', 'success', 'warning', 'danger', 'info']),
     size: React.PropTypes.oneOf(['default', 'small', 'medium', 'large', 'extra-large']),
     position: React.PropTypes.oneOf(['top', 'right', 'bottom', 'left'])
   },
