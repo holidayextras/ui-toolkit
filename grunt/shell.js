a81b9128a9df16c77f52c0e721aa28b4031b79a7
Jodie Doubleday <jodie.doubleday@holidayextras.com>
2015-06-01 15:03:32 +0100

    fixing weird formatting issues

--- grunt/shell.js
+++ grunt/shell.js
@@ -1,17 +1,17 @@
 module.exports = {
   ulimit: {
-    command: "ulimit -n 9999",
+    command: 'ulimit -n 9999'
   },
   buildDist: {
     command: "./node_modules/.bin/browserify -g [ ./node_modules/reactify --extension '.jsx' ] ./index.js --standalone ui-toolkit > ./dist/ui-toolkit.standalone.js"
   },
   docsBuild: {
-    command: 'cd docs; npm run build; cd -',
+    command: 'cd docs; npm run build; cd -'
   },
   docsStart: {
-    command: 'cd docs; npm start; cd -',
+    command: 'cd docs; npm start; cd -'
   },
   docsView: {
-    command: 'open http://localhost:4000',
+    command: 'open http://localhost:4000'
   }
 };
--- grunt/watch.js
+++ grunt/watch.js
@@ -1,9 +1,8 @@
 module.exports = {
   docs: {
-      files: ['docs/src/**/*.js*', 'src/**/*.js*'],
-      // files: ['docs/**/*.jsx', 'src/**/*.jsx', 'src/**/*.js'],
-      tasks: ['docs-build'],
-      options: {
+    files: ['docs/src/**/*.js*', 'src/**/*.js*'],
+    tasks: ['docs-build'],
+    options: {
       livereload: true
     }
   },
