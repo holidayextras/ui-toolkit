0d4eb64867c32c1e3cb7711269cac9a615ec3830
Jodie Doubleday <jodie.doubleday@holidayextras.com>
2015-06-05 12:38:50 +0100

    getting linting working and fixing the files

--- src/components/rating/code/templates/ratingTemplate.jsx
+++ src/components/rating/code/templates/ratingTemplate.jsx
@@ -1,16 +1,14 @@
-var React = require('react');
-
 module.exports = function(props) {
 
   var $rating = [];
   var $blankRating = [];
-  
+
   for(var i = 0; i < props.rating; i++) {
-    $rating.push( (!!props.children) ? props.children : <span className="rating-icon" /> );
+    $rating.push( props.children ? <span className="rating-icon" /> : props.children);
   }
-  
+
   if(props.outOf && props.blankIcon){
-    for(var j = 0; j < ( props.outOf-props.rating ); j++) {
+    for(var j = 0; j < ( props.outOf - props.rating ); j++) {
       $blankRating.push( props.blankIcon );
     }
   }
