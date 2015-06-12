78bbbaaafa8d03be0161daa9c86a0fc1bc4f3897
jackdcrawford <jackcrawford19@gmail.com>
2015-06-12 10:43:14 +0100

    UXUI-239 fixing build error no need for supervisor adding test files

--- src/components/icon-list-item/__tests__/IconListItemComponent-test.js
+++ src/components/icon-list-item/__tests__/IconListItemComponent-test.js
@@ -1,7 +1,5 @@
 /** @jsx React.DOM */
 
-jest.dontMock('../code/views/IconListItemComponentView.jsx');
-
 var React = require('react/addons');
 var IconListItemComponent = require('../code/views/IconListItemComponentView.jsx');
 var TestUtils = React.addons.TestUtils;
@@ -9,7 +7,7 @@ var TestUtils = React.addons.TestUtils;
 describe('IconListItemComponent', function() {
 
   it('is an element', function() {
-    expect(TestUtils.isElement(<IconListItemComponent />)).toBeTruthy();
+    assert.ok(TestUtils.isElement(<IconListItemComponent />));
   });
 
 });
--- src/components/icon-list/__tests__/IconListComponent-test.js
+++ src/components/icon-list/__tests__/IconListComponent-test.js
@@ -1,15 +1,90 @@
 /** @jsx React.DOM */
 
-jest.dontMock('../code/views/IconListComponentView.jsx');
+var IconList = require('../code/views/iconListComponentView.jsx');
+var IconListItem = require('../../icon-list-item/code/views/iconListItemComponentView.jsx');
 
-var React = require('react/addons');
-var IconListComponent = require('../code/views/IconListComponentView.jsx');
-var TestUtils = React.addons.TestUtils;
-
-describe('IconListComponent', function() {
+describe('iconListComponent ', function() {
 
   it('is an element', function() {
-    expect(TestUtils.isElement(<IconListComponent />)).toBeTruthy();
+    assert.ok(TestUtils.isElement(<IconList />));
   });
+
+  it('should render an unordered list', function() {
+    var iconListInstance = TestUtils.renderIntoDocument(
+      <IconList>
+        <IconListItem icon="check">Positive</IconListItem>
+        <IconListItem icon="close">Negative</IconListItem>
+      </IconList>
+    );
+
+    var renderedIconList = TestUtils.findRenderedDOMComponentWithClass(ul, 'component-icon-list');
+    assert.equal(renderedIconList.getDOMNode().nodeName, )
+
+    // assert.equal(React.findDOMNode(instance).nodeName, 'DIV');
+    // assert.ok(React.findDOMNode(instance).className.match(/\bbtn-group\b/));
+    // var renderedButton = TestUtils.findRenderedDOMComponentWithClass(button, 'component-button');
+    // assert.equal(renderedButton.getDOMNode().textContent, 'Book Now');
+
+  });
+
+
+
+  // it('should render a primary button', function() {
+  //   var primaryButton = TestUtils.renderIntoDocument(
+  //     <ButtonView purpose="primary">Book Now</ButtonView>
+  //   );
+
+  //   var renderedButton = TestUtils.findRenderedDOMComponentWithClass(primaryButton, 'primary');
+  //   assert.isDefined(renderedButton);
+
+  // });
+
+  // it('should render a default button', function() {
+  //   var defaultButton = TestUtils.renderIntoDocument(
+  //     <ButtonView size="default">Book Now</ButtonView>
+  //   );
+
+  //   var renderedButton = TestUtils.findRenderedDOMComponentWithClass(defaultButton, 'default');
+  //   assert.isDefined(renderedButton);
+
+  // });
+
+  // it('should render a medium button', function() {
+  //   var mediumButton = TestUtils.renderIntoDocument(
+  //     <ButtonView size="medium">Book Now</ButtonView>
+  //   );
+
+  //   var renderedButton = TestUtils.findRenderedDOMComponentWithClass(mediumButton, 'medium');
+  //   assert.isDefined(renderedButton);
+
+  // });
+
+  // it('should render a large button', function() {
+  //   var largeButton = TestUtils.renderIntoDocument(
+  //     <ButtonView size="large">Book Now</ButtonView>
+  //   );
+
+  //   var renderedButton = TestUtils.findRenderedDOMComponentWithClass(largeButton, 'large');
+  //   assert.isDefined(renderedButton);
+
+  // });
+
+  // it('should render a small secondary button', function() {
+  //   var smallSecondaryButton = TestUtils.renderIntoDocument(
+  //     <ButtonView size="small" purpose="secondary">Book Now</ButtonView>
+  //   );
+
+  //   var renderedButton = TestUtils.findRenderedDOMComponentWithClass(smallSecondaryButton, 'small secondary');
+  //   assert.isDefined(renderedButton);
+
+  // });
+
+  // it('should render as an anchor if href is present', function() {
+  //   var anchor = TestUtils.renderIntoDocument(
+  //     <ButtonView href='http://www.ui-toolkit.com'>Go to Toolkit</ButtonView>
+  //   );
+
+  //   var renderedButton = TestUtils.findRenderedDOMComponentWithTag(anchor, 'a');
+  //   assert.isDefined(renderedButton);
 
 });
