8bfb40164ad2c61be8510c574d39a77c375b103f
Peter Schmalfeldt <peter@manifestinteractive.com>
2015-06-19 05:06:52 -0400

    Fixing logic to check for any input rather than a month / day.  Also cleaned up with latest lint tests and verified it passed unit existing tests.

--- src/components/credit-card-expire/code/views/creditCardExpireView.jsx
+++ src/components/credit-card-expire/code/views/creditCardExpireView.jsx
@@ -20,7 +20,7 @@ module.exports = React.createClass({
     return {
       error: null,
       valid: true
-    }
+    };
   },
 
   getDefaultProps: function() {
@@ -33,29 +33,30 @@ module.exports = React.createClass({
     };
   },
 
-  handleChange: function(elm){
+  handleChange: function(e){
 
     if( !this.formatting){
       this.formatting = true;
       Payment.formatCardExpiry(document.querySelector('.credit-card-expire-input'));
     }
 
-    var expires = elm.target.value;
-    var is_valid = true;
+    var expires = e.target.value;
+    var isValid = true;
     var self = this;
 
     if(expires.length > 0){
+      isValid = false;
       var date = expires.split(' / ');
       if(date.length === 2){
-        is_valid = Payment.fns.validateCardExpiry(date[0], date[1]);
+        isValid = Payment.fns.validateCardExpiry(date[0], date[1]);
       }
     }
 
     clearTimeout(this.intent);
     this.intent = setTimeout(function(){
       self.setState({
-        valid: is_valid,
-        error: (is_valid) ? null : self.props.errorMessage
+        valid: isValid,
+        error: (isValid) ? null : self.props.errorMessage
       });
     }, 500);
   },
