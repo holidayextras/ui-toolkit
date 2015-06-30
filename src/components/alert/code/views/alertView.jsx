24b88e4d13f65a22e8e5429cfae3fa3f3d67a04e
Jodie Doubleday <jodie.doubleday@holidayextras.com>
2015-06-30 16:40:30 +0100

    solve react linting issue

--- src/components/alert/code/views/alertView.jsx
+++ src/components/alert/code/views/alertView.jsx
@@ -4,7 +4,7 @@ module.exports = React.createClass({
 
   propTypes: {
     purpose: React.PropTypes.oneOf(['default', 'primary', 'secondary', 'success', 'warning', 'danger', 'info']),
-    size: React.PropTypes.oneOf(['default', 'small', 'medium', 'large', 'extra-large']),
+    size: React.PropTypes.oneOf(['default', 'small', 'medium', 'large', 'extra-large'])
   },
 
   render: function() {
