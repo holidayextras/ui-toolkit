91c06bb1f82ff85b0d32e4228a8897cbc7d8280a
Jodie Doubleday <jodie.doubleday@holidayextras.com>
2015-07-01 15:55:47 +0100

    removing scripts dev and dist files

--- src/components/flag/scripts/build-dist.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ -z "$VARIANT" ]; then VARIANT=''; fi
-if [ -z "$TEMPLATE_LANG" ]; then TEMPLATE_LANG='en'; fi
-
-echo "Building package for demo. Variant: '$VARIANT', language: '$TEMPLATE_LANG'."
-
-browserify -t [ redirectify --dir "$VARIANT" ] -t reactify index.js --standalone ui-component-flag > dist/ui-component-flag-standalone.js
