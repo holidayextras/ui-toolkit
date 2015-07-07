2bc0aa6561d2c02398e142046103c912b1c08a18
Jodie Doubleday <jodie.doubleday@holidayextras.com>
2015-07-07 17:54:35 +0100

    remove double quotes

--- src/components/quote/code/templates/quoteTemplate.jsx
+++ src/components/quote/code/templates/quoteTemplate.jsx
@@ -10,7 +10,7 @@ module.exports = function() {
   return (
       <q className={classNames(classes)} cite={this.props.cite} itemScope itemType="http://schema.org/CreativeWork" itemProp="text">
         {this.props.children}
-        {(this.props.cite) ? <meta itemProp="citation" content="{this.props.cite}" /> : null}
+        {(this.props.cite) ? <meta itemProp="citation" content={this.props.cite} /> : null}
         <span itemProp="author" itemScope itemType="http://schema.org/Person">
           <meta itemProp="name" content={this.props.author} />
           <meta itemProp="jobTitle" content={this.props.role} />
