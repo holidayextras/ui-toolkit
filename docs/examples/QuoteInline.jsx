7acbf49d1d1f5c75dd70eb4319908f9d46b06e4a
Jodie Doubleday <jodie.doubleday@holidayextras.com>
2015-07-07 13:02:48 +0100

    adding extras inline styles

--- docs/examples/QuoteInline.jsx
+++ docs/examples/QuoteInline.jsx
@@ -1,6 +1,7 @@
 var example = (
   <div>
-    <UIToolkit.Quote type="inline" author="Gandalf" role="Captain of the Starship Enterprise" cite="The Internet" purpose="success">Use the force Harry</UIToolkit.Quote>
+    <p>Gandalf, Captain of the starship enterprise said <UIToolkit.Quote type="inline" author="Gandalf" role="Captain of the Starship Enterprise" cite="The Internet" purpose="success">Use the force Harry</UIToolkit.Quote>
+    </p>
   </div>
 );
 
