23dd58bd46f30e2076a3abacc6dff93c4682a7c7
Reece Millsom <reece.millsom@HX2451T1.local>
2015-07-16 17:06:50 +0100

    Created a div for the component

--- src/components/countdown/code/templates/countdownTemplate.jsx
+++ src/components/countdown/code/templates/countdownTemplate.jsx
@@ -1,8 +1,11 @@
 var React = require('react');
 
 module.exports = function(props) {
+
+  countdown equals end date - start date
   return (
+    <div classname="component-countdown">
 
-
+    </div>
   );
 };
