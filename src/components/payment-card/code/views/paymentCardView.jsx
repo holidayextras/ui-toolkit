ea85e2aec4130985361f5c821b29468b053bf40a
Jodie Doubleday <jodie.doubleday@holidayextras.com>
2015-07-16 11:29:47 +0100

    adding tests

--- src/components/payment-card/code/views/paymentCardView.jsx
+++ src/components/payment-card/code/views/paymentCardView.jsx
@@ -4,7 +4,6 @@ var React = require('react');
 module.exports = React.createClass({
   propTypes: {
     type: React.PropTypes.oneOf(['amazon','amex','apple','cirrus','delta','directdebit','discover','electron','google','maestro','mastercard','paym','paypal','sage','sepa','solo','switch','ukash','visa','visadebit','western union']),
-    size: React.PropTypes.oneOf(['default', 'small', 'medium', 'large', 'extra-large']),
   },
 
   render: function() {
