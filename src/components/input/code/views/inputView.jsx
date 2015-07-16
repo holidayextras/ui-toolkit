02b3229c6315518c4172ef9da90f155d9b515f20
Cameron Viner <cameron.viner@holidayextras.com>
2015-07-16 14:59:27 +0100

    regex change

--- src/components/input/code/views/inputView.jsx
+++ src/components/input/code/views/inputView.jsx
@@ -17,7 +17,7 @@ module.exports = React.createClass({
     readOnly: React.PropTypes.bool,
     required: React.PropTypes.bool,
     valid: React.PropTypes.bool,
-    validator: React.PropTypes.any,
+    validator: React.PropTypes.instanceOf(RegExp),
     errorMessage: React.PropTypes.string
   },
 
@@ -49,22 +49,12 @@ module.exports = React.createClass({
     var self = this;
     var value = (e.target) ? e.target.value : null;
 
-    console.log(value);
-    console.log(typeof value);
-    // console.log(self.props.validator);
-
-
     if(value !== '' && typeof self.props.validator !== 'undefined'){
-      console.log('start');
-      console.log(RegExp(self.props.validator).test(value))
-      isValid = new RegExp(/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i).test('cameron.viner@gmail.com')
-      console.log(isValid);
-      console.log('end');
+      isValid = self.props.validator.test(value)
     }
 
     if( !isValid){
       error = self.props.errorMessage;
-      console.log("banana");
     }
 
     clearTimeout(this.intent);
