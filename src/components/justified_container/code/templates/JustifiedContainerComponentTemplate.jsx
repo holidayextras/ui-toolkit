3a0e0b08807e0a4a71b39a13be49a37421c24372
Jodie Doubleday <jodie.doubleday@holidayextras.com>
2015-07-21 13:52:55 +0100

    remname some files and remove styles from components and place into docs

--- src/components/justified_container/code/templates/JustifiedContainerComponentTemplate.jsx
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-
-module.exports = function(props) {
-  return (
-    <div style={props.styles} className="component-justified-container">
-      {props.children}
-    </div>
-  );
-};
