94472178444c9e1bf016a5f3c370b3543d04ad4b
Jodie Doubleday <jodie.doubleday@holidayextras.com>
2015-08-19 12:16:53 +0100

    update tests to remove this. from all of it

--- src/components/anchor/__tests__/anchor-test.js
+++ src/components/anchor/__tests__/anchor-test.js
@@ -6,83 +6,92 @@ var assert = require('chai').assert;
 var Anchor = require('../code/views/anchorView.jsx');
 
 describe('Anchor', function() {
+
+  var data = null;
+  var title = null;
+  var href = null;
+  var clickHandler = null;
+  var target = null;
+  var instance = null;
+  var anchorDomNode = null;
+
   beforeEach(function(){
-    this.data = {
+    data = {
       'foo': 'bar',
       'x-y-z': 'zyx'
     };
-    this.title = 'test';
-    this.href = 'google.com';
-    this.clickHandler = sinon.spy();
-    this.target = 'blank';
+    title = 'test';
+    href = 'google.com';
+    clickHandler = sinon.spy();
+    target = 'blank';
 
-    this.instance = TestUtils.renderIntoDocument(
-      <Anchor data={this.data} title={this.title} href={this.href} handleClick={this.clickHandler} target={this.target} foo="bar">test anchor</Anchor>
+    instance = TestUtils.renderIntoDocument(
+      <Anchor data={data} title={title} href={href} handleClick={clickHandler} target={target} foo="bar">test anchor</Anchor>
     );
 
-    var renderedAnchor = TestUtils.findRenderedDOMComponentWithTag(this.instance, 'a');
-    this.anchorDomNode = renderedAnchor.getDOMNode();
+    var renderedAnchor = TestUtils.findRenderedDOMComponentWithTag(instance, 'a');
+    anchorDomNode = renderedAnchor.getDOMNode();
   });
 
   it('is an element', function(){
-    assert(TestUtils.isCompositeComponent(this.instance));
+    assert(TestUtils.isCompositeComponent(instance));
   });
 
   describe('with content', function(){
     it('should render an anchor', function(){
-      assert.equal(this.anchorDomNode.nodeName, 'A');
+      assert.equal(anchorDomNode.nodeName, 'A');
     });
 
     describe('with data attributes', function() {
       it('spreads them and prefixes with \'data\'', function(){
-        assert.equal(this.anchorDomNode.getAttribute('data-foo'), 'bar');
-        assert.equal(this.anchorDomNode.getAttribute('data-x-y-z'), 'zyx');
+        assert.equal(anchorDomNode.getAttribute('data-foo'), 'bar');
+        assert.equal(anchorDomNode.getAttribute('data-x-y-z'), 'zyx');
       });
     });
 
     it('should have the correct title from props', function(){
-      assert.equal(this.anchorDomNode.getAttribute('title'), this.title);
+      assert.equal(anchorDomNode.getAttribute('title'), title);
     });
 
     describe('with handleClick handler', function(){
       describe('on click', function(){
         beforeEach(function(){
-          TestUtils.Simulate.click(this.anchorDomNode);
+          TestUtils.Simulate.click(anchorDomNode);
         });
 
         it('should call the handler', function(){
-          assert(this.clickHandler.calledOnce);
+          assert(clickHandler.calledOnce);
         });
       });
     });
 
     it('should have the correct target from props', function(){
-      assert.equal(this.anchorDomNode.getAttribute('target'), '_' + this.target);
+      assert.equal(anchorDomNode.getAttribute('target'), '_' + target);
     });
 
   });
 
   describe('without content', function(){
     beforeEach(function(){
-      this.instance = TestUtils.renderIntoDocument(
-        <Anchor data={this.data} title={this.title} href={this.href} handleClick={this.clickHandler} />
+      instance = TestUtils.renderIntoDocument(
+        <Anchor data={data} title={title} href={href} handleClick={clickHandler} />
       );
     });
     it('does not render', function(){
-      assert.equal(TestUtils.scryRenderedDOMComponentsWithTag(this.instance, 'a').length, 0);
+      assert.equal(TestUtils.scryRenderedDOMComponentsWithTag(instance, 'a').length, 0);
     });
   });
 
   describe('without a href', function(){
     beforeEach(function(){
-      this.instance = TestUtils.renderIntoDocument(
-        <Anchor data={this.data} title={this.title} handleClick={this.clickHandler} >Content</Anchor>
+      instance = TestUtils.renderIntoDocument(
+        <Anchor data={data} title={title} handleClick={clickHandler} >Content</Anchor>
       );
-      var renderedAnchor = TestUtils.findRenderedDOMComponentWithTag(this.instance, 'a');
-      this.anchorDomNode = renderedAnchor.getDOMNode();
+      var renderedAnchor = TestUtils.findRenderedDOMComponentWithTag(instance, 'a');
+      anchorDomNode = renderedAnchor.getDOMNode();
     });
     it('defaults to #', function(){
-      assert.equal(this.anchorDomNode.getAttribute('href'), '#');
+      assert.equal(anchorDomNode.getAttribute('href'), '#');
     });
   });
 });
