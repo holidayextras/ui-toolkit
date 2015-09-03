12ccad4fcbc78b8a0142a5ea757cbdae74c546fe
Jodie Doubleday <jodie.doubleday@holidayextras.com>
2015-09-03 14:15:28 +0100

    trying a new way of linting

--- grunt/less.js
+++ grunt/less.js
@@ -1,8 +1,13 @@
 module.exports = {
   all: {
-    // options: {
-    //   compress: true
-    // },
+    options: {
+      compress: true
+    },
+    files: {
+      'dist/toolkit.css': 'src/less/toolkit.less'
+    }
+  },
+  lint: {
     files: {
       'dist/toolkit.css': 'src/less/toolkit.less'
     }
