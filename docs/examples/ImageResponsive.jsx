fc11a492efd4884af7c2f0d26c19188babf1ff98
Jodie Doubleday <jodie.doubleday@holidayextras.com>
2015-10-08 14:37:22 +0100

    fixing the responsive images in docs

--- docs/examples/ImageResponsive.jsx
+++ docs/examples/ImageResponsive.jsx
@@ -1,7 +1,7 @@
 var image = {
-  srcSet: '/assets/component-image/panda2000.jpg 2000w, /assets/component-image/panda1500.jpg 1500w, /assets/component-image/panda1000.jpg 1000w, /assets/component-image/panda500.jpg 500w',
+  srcSet: 'https://open-source.holidayextras.com/ui-toolkit/images/panda2000.jpg 2000w, https://open-source.holidayextras.com/ui-toolkit/images/panda1500.jpg 1500w, https://open-source.holidayextras.com/ui-toolkit/images/panda1000.jpg 1000w, https://open-source.holidayextras.com/ui-toolkit/images/panda500.jpg 500w',
   src: '/assets/component-image/panda.jpg',
-  alt: 'Delicious strawberry cheesecake'
+  alt: 'A cute panda'
 };
 
 var example = (
