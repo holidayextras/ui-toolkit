718ba6a8b0769fa5534f4392d5790872cb77d06b
Rob Huzzey <robert.huzzey@holidayextras.com>
2015-10-30 19:03:34 +0000

    Improved basket item to have more flexibility

--- src/components/basket-item/basket-item.jsx
+++ src/components/basket-item/basket-item.jsx
@@ -4,27 +4,70 @@ var React = require('react');
 module.exports = React.createClass({
   propTypes: {
     currencySymbol: React.PropTypes.string,
+    freeText: React.PropTypes.string,
+    removeText: React.PropTypes.string,
     price: React.PropTypes.number,
+    title: React.PropTypes.node,
+    toggleDescription: React.PropTypes.bool,
+    handleRemove: React.PropTypes.func,
     children: React.PropTypes.node
+  },
+  getInitialState: function() {
+    return {
+      descriptionVisibility: !this.props.toggleDescription
+    };
   },
   getDefaultProps: function() {
     return {
-      currencySymbol: '£'
+      currencySymbol: '£',
+      freeText: 'FREE',
+      removeText: 'remove',
+      toggleDescription: false,
+
     };
   },
-  render: function() {
-    var priceNode;
-    if (this.props.price) {
-      priceNode = (
-        <div className="component-basket-item-price">
-          <span className="component-currency">{this.props.currencySymbol}</span>{this.props.price}
-        </div>
-      );
+  toggleDescriptionVisibility: function() {
+    this.setState({descriptionVisibility: !this.state.descriptionVisibility});
+  },
+  titleNode: function() {
+    if (!this.props.title) return;
+    if (React.isValidElement(this.props.title)) return this.props.title;
+    if (this.props.toggleDescription) return (<a onClick={this.toggleDescriptionVisibility}>{this.props.title}</a>);
+    return this.props.title;
+  },
+  priceNode: function() {
+    if (this.props.price === null) return;
+    if (this.props.price === 0) {
+      return (<div className="component-basket-item-price">{this.props.freeText}</div>);
     }
     return (
+      <div className="component-basket-item-price">
+        <span className="component-currency">{this.props.currencySymbol}</span><span className="component-price">{this.props.price}</span>
+      </div>
+    );
+  },
+  removeNode: function() {
+    if (!this.props.handleRemove) return;
+    return (
+      <div className="component-basket-item-remove">
+        <a onClick={this.props.handleRemove}>{this.props.removeText}</a>
+      </div>
+    );
+  },
+  render: function() {
+    var descriptionStyle = {
+      "display": this.state.descriptionVisibility ? "block" : "none"
+    };
+    return (
       <div className="component-basket-item">
-        <div className="component-basket-item-description">{this.props.children}</div>
-        {priceNode}
+        <div className="component-basket-row">
+          <div className="component-basket-item-title">{this.titleNode()}</div>
+          {this.priceNode()}
+        </div>
+        <div className="component-basket-row">
+          <div className="component-basket-item-description" style={descriptionStyle}>{this.props.children}</div>
+          {this.removeNode()}
+        </div>
       </div>
     );
   }
