09730ef97921013f300e5290fe9cce7769aec55d
Jodie Doubleday <jodie.doubleday@holidayextras.com>
2015-11-06 14:06:59 +0000

    fixing broken test

--- test/components/button-test.jsx
+++ test/components/button-test.jsx
@@ -18,7 +18,7 @@ describe('ButtonComponent', function() {
 
   it('should render a primary button', function() {
     var primaryButton = TestUtils.renderIntoDocument(
-      <ButtonView purpose="primarry">Book Now</ButtonView>
+      <ButtonView purpose="primary">Book Now</ButtonView>
     );
 
     var renderedButton = TestUtils.findRenderedDOMComponentWithClass(primaryButton, 'primary');
