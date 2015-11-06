b9fec4a5742543ae4ba3be6c78f9f2a519149dd7
Paul Clarke <paul.clarke@holidayextras.com>
2015-11-06 12:33:29 +0000

    build all of dist on install

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,15 @@
+SCRIPTFOLDER=$( dirname "${BASH_SOURCE[0]}" )
+CASESCRIPT=${SCRIPTFOLDER}/toPascalCase.js
 SRCFOLDER=src/components
 DISTFOLDER=dist/components
+DISTINDEX=dist/index.js
+echo 'var UIToolkit = module.exports = {};' > ${DISTINDEX}
+echo '// Custom Components' >> ${DISTINDEX}
 for tool in $( ls -1 $SRCFOLDER ); do
   [ -e "$SRCFOLDER/$tool/$tool.jsx" ] || continue
   mkdir -p $DISTFOLDER/$tool
   babel $SRCFOLDER/$tool/$tool.jsx -o $DISTFOLDER/$tool/index.js
+  echo "UIToolkit.$( ${CASESCRIPT} ${tool}) = require('./components/${tool}');" >> ${DISTINDEX}
 done
+ulimit -n 9999
+grunt build
