e327e060526eea58390377f1bf6c666b12765fdf
Richard Stevens <richard.stevens@holidayextras.com>
2015-11-09 11:16:49 +0000

    Updating stepper to allow new step value

--- src/components/stepper/stepper.jsx
+++ src/components/stepper/stepper.jsx
@@ -8,30 +8,33 @@ module.exports = React.createClass({
 
   propTypes: {
     value: React.PropTypes.number,
-    onChange: React.PropTypes.func,
+    onChange: React.PropTypes.func.isRequired,
     minValue: React.PropTypes.number,
     maxValue: React.PropTypes.number,
+    stepValue: React.PropTypes.number,
     incrementDisplayString: React.PropTypes.string,
-    decrementDisplayString: React.PropTypes.string
+    decrementDisplayString: React.PropTypes.string,
+    id: React.PropTypes.string,
+    label: React.PropTypes.string
   },
 
   getDefaultProps: function() {
     return {
       value: 0,
+      stepValue: 1,
       incrementDisplayString: '+',
-      decrementDisplayString: '-',
-      onChange: function() {}
+      decrementDisplayString: '-'
     };
   },
 
   decrement: function() {
     if (!this.canDecrement()) return;
-    this.props.onChange(this.props.value - 1);
+    this.props.onChange(this.props.value - this.props.stepValue);
   },
 
   increment: function() {
     if (!this.canIncrement()) return;
-    this.props.onChange(this.props.value + 1);
+    this.props.onChange(this.props.value + this.props.stepValue);
   },
 
   canIncrement: function() {
@@ -51,7 +54,7 @@ module.exports = React.createClass({
           <Button handleClick={this.decrement} type="button" disabled={!this.canDecrement()}>{this.props.decrementDisplayString}</Button>
         </span>
 
-        <Input type="text" key={this.props.value} readOnly={true}>{this.props.value.toString()}</Input>
+        <Input type="text" id={this.props.id} key={this.props.value} readOnly={true} label={this.props.label}>{this.props.value.toString()}</Input>
 
         <span className="button-container">
           <Button handleClick={this.increment} type="button" disabled={!this.canIncrement()}>{this.props.incrementDisplayString}</Button>
