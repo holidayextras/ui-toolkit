58d3a48a644af0dea6b47384041bb400f811e53d
samsmarthx <sam.smart@holidayextras.com>
2015-11-17 15:37:09 +0000

    OPS-1423 change to boolean

--- docs/examples/Select-multiple.jsx
+++ docs/examples/Select-multiple.jsx
@@ -1,6 +1,6 @@
 var example = (
   <div>
-    <UIToolkit.Select label='Destination' multiple={true}>
+    <UIToolkit.Select label='Destination' multiple={{true}}>
       <option>Country</option>
       <option>Germany</option>
       <option>Spain</option>
