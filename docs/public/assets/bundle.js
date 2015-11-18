c83123c1c56957d18331f4e97880f3babbf98de4
samsmarthx <sam.smart@holidayextras.com>
2015-11-18 12:58:50 +0000

    Build dist files

--- docs/public/assets/bundle.js
+++ docs/public/assets/bundle.js
@@ -31733,7 +31733,7 @@ module.exports = require('./lib/React');
 },{"./lib/React":181}],314:[function(require,module,exports){
 module.exports={
   "name": "ui-toolkit",
-  "version": "0.24.3",
+  "version": "0.26.0",
   "description": "UI Toolkit",
   "license": "MIT",
   "main": "index.js",
