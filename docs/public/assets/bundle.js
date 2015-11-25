08bc7a0990dfe4305e089cea51f6ac5ef22a7922
Jodie Doubleday <jodie.doubleday@holidayextras.com>
2015-11-25 11:12:36 +0000

    there is a problem in the CSS where all children are displayed table, this stops that

--- docs/public/assets/bundle.js
+++ docs/public/assets/bundle.js
@@ -27765,7 +27765,7 @@ module.exports = require('./lib/React');
 },{"./lib/React":146}],279:[function(require,module,exports){
 module.exports={
   "name": "ui-toolkit",
-  "version": "0.26.2",
+  "version": "0.26.3",
   "description": "UI Toolkit",
   "license": "MIT",
   "main": "index.js",
