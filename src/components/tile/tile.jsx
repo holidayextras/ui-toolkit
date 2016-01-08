98c3e11b49966e35db5b5647a5649e7f1f6c5777
Paul Clarke <paul.clarke@holidayextras.com>
2016-01-08 16:17:32 +0000

    Add optional key to tile

--- src/components/tile/tile.jsx
+++ src/components/tile/tile.jsx
@@ -4,14 +4,19 @@ var Image = require('../image');
 
 module.exports = React.createClass({
   propTypes: {
+    key: React.PropTypes.number,
     children: React.PropTypes.any,
     image: React.PropTypes.object.isRequired,
     title: React.PropTypes.string.isRequired
   },
 
   render: function() {
+    const wrapperProps = {
+      className: "component-tile",
+      key: this.props.key
+    };
     return (
-      <div className="component-tile">
+      <div {...wrapperProps}>
         <Image {...this.props.image} />
         <div className="caption">
           <h4>{this.props.title}</h4>
