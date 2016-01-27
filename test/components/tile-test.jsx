b45be14b68c2c2c1969dd7ca036c77216a7cd4b0
Jodie Doubleday <jodie.doubleday@holidayextras.com>
2016-01-27 12:05:35 +0000

    add a test for tile

--- test/components/tile-test.jsx
+++ test/components/tile-test.jsx
@@ -6,17 +6,30 @@ var TileComponent = require('../../src/components/tile/tile.jsx');
 
 describe('TileComponent', function() {
 
-  it('is an element', function() {
-    var img = {
+  var img
+
+  beforeEach(function() {
+    img = {
       src: 'foo',
       alt: 'bar'
     };
+  });
 
+  it('is an element', function() {
     var component = TestUtils.renderIntoDocument(
-    	<TileComponent image={img} title="title" />
+      <TileComponent image={img} />
     );
-    
+
     assert.isDefined(component);
+  });
+
+  it('should render a tile/card with a title', function() {
+    var tile = TestUtils.renderIntoDocument(
+      <TileComponent image={img} title="foo" >You have been successful</TileComponent>
+    );
+    var h4s = TestUtils.scryRenderedDOMComponentsWithTag(tile, 'h4');
+    assert.equal(h4s.length, 1);
+    assert.equal(h4s[0].textContent, 'foo');
   });
 
 });
