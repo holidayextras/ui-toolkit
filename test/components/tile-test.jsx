151a4a78779253a61cc7f2fee52dc366fc3c2019
Luke Hansell <luke.hansell@holidayextras.com>
2016-06-21 10:17:45 +0100

    allowing spread operator on main tile div

--- test/components/tile-test.jsx
+++ test/components/tile-test.jsx
@@ -3,6 +3,7 @@ var React = require('react');
 var TestUtils = require('react-addons-test-utils');
 var assert = require('chai').assert;
 var TileComponent = require('../../src/components/tile/tile.jsx');
+var sinon = require('sinon');
 
 describe('TileComponent', function() {
 
@@ -39,4 +40,14 @@ describe('TileComponent', function() {
     assert.isDefined(renderedTile);
   });
 
+  it('should render any extra props passed to it on the containing div', function() {
+    var onClickHandler = sinon.stub();
+    var tile = TestUtils.renderIntoDocument(
+      <TileComponent image={img} title="foo" onClick={onClickHandler}/>
+    );
+    var renderedTile = TestUtils.findRenderedDOMComponentWithClass(tile, 'component-tile');
+    TestUtils.Simulate.click(renderedTile);
+
+    assert(onClickHandler.called);
+  });
 });
