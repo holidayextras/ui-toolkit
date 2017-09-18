e4a83af4fab3f8ef5039bdfe85a30e2c526c90ff
Vasil Bachev <vasil.bachev@holidayextras.com>
2017-09-18 15:38:55 +0300

    MOB-77 Rating component: add key props to the rating icons

--- test/components/rating-test.jsx
+++ test/components/rating-test.jsx
@@ -23,10 +23,28 @@ describe('Rating', function() {
       });
     });
 
+    describe('with a child prop that is a component', function() {
+      it('is an element', function() {
+        var component = TestUtils.renderIntoDocument(
+          <Rating rating={4}><div>X</div></Rating>
+        );
+        assert.isDefined(component);
+      });
+    });
+
     describe('with a blank icon prop', function() {
       it('is an element', function() {
         var component = TestUtils.renderIntoDocument(
           <Rating rating={4} outOf={5} blankIcon="X" />
+        );
+        assert.isDefined(component);
+      });
+    });
+
+    describe('with a blank icon prop that is a component', function() {
+      it('is an element', function() {
+        var component = TestUtils.renderIntoDocument(
+          <Rating rating={4} outOf={5} blankIcon={<div>X</div>} />
         );
         assert.isDefined(component);
       });
